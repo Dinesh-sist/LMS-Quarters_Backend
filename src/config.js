693595bef55ae8b414e3e44584df17c7d6a6c3f1
Dhineshthiru17 <146512715+Dhineshthiru17@users.noreply.github.com>
2026-05-18 11:49:20 +0530

    mmmm

--- src/config.js
+++ src/config.js
@@ -20,11 +20,18 @@ module.exports = {
   db: {
     server: env("DB_SERVER", "localhost"),
     port: Number(env("DB_PORT", "1433")),
-    database: env("DB_DATABASE", "LMSQuarters"),
-    user: env("DB_USER", "sa"),
-    password: env("DB_PASSWORD", ""),
+    database: env("DB_DATABASE", "lmsQUARTERS"),
+    user: env("DB_USER", "Dhinesh"),
+    password: env("DB_PASSWORD", "Dhinesh1714"),
     encrypt: envBool("DB_ENCRYPT", false),
     trustServerCertificate: envBool("DB_TRUST_SERVER_CERT", true)
   }
 };
+
+
+
+
+
+
+
 
